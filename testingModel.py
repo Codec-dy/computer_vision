8c8275696c329ebbb8eb4ed12e0079fd2e491807
Codec-dy <70746104+Codec-dy@users.noreply.github.com>
2022-08-04 17:06:08 +0400

    Add files via upload

--- testingModel.py
+++ testingModel.py
@@ -1,6 +1,10 @@
 from keras.models import load_model
 from keras_preprocessing import image
 import numpy as np
+import os
+import cv2 as cv
+import mysql.connector as sql
+from mysql.connector import Error
 
 #Load our Model created using trainingModel.py
 model = load_model('CNN.model')
@@ -19,13 +23,45 @@ def prepare(filepath):
 
 
 try:
-    value = model.predict(prepare('kente/kente.jpg'))
+    user_image = 'car.jpg'
+    value = model.predict(prepare(user_image))
     index = value[0].tolist().index(1)
     print('This is a ' + categories[index])
+    orb = cv.ORB_create(nfeatures=4000)
+    databaseImages = os.getcwd() + '\DatabaseImages' + '\%s' %categories[index]
 
-except:
+    for image_to_search in os.listdir(databaseImages):
+        print('DatabaseImages/%s/%s' % (categories[index], image_to_search))
+
+        img_from_database = cv.imread('DatabaseImages/%s/%s' % (categories[index], image_to_search), 0)
+
+        user_image_gray = cv.read(user_image,0)
+        img_from_database_resized = cv.resize(img_from_database, (400, 400))
+        user_image_resized = cv.resize(user_image_gray,(400,400))
+
+        """ keypoints finding && """
+        keypoints_1, descriptor_1 = orb.detectAndCompute(user_image_resized, None)
+        keypoints_2, descriptor_2 = orb.detectAndCompute(img_from_database, None)
+
+        bf = cv.BFMatcher()
+        matches = bf.knnMatch(descriptor_1, descriptor_2, k=2)
+
+        """ matches occuring"""
+        good_keypoint = []
+
+        for m, n in matches:
+            if m.distance < 0.75 * n.distance:
+                good_keypoint.append([m])
+        print(len(good_keypoint))
+        compared_images = cv.drawMatchesKnn(user_image_resized, keypoints_1, img_from_database_resized,
+                                            keypoints_2, good_keypoint, None, flags=2)
+
+
+        cv.imshow('img3', compared_images)
+
+except Name:
     print('The product does not exist')
-
+    print(err)
 finally:
     print(value)
 
